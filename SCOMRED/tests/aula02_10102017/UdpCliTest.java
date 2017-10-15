b9f340da2bd6d283c9be534ef260ec37a4c29d62
SergioValente <sergio134@sapo.pt>
2017-10-15 21:30:35 +0100

    add a mock test file to SCOMRED/tests to test GitHub functionality

---
+++ SCOMRED/tests/aula02_10102017/UdpCliTest.java
@@ -0,0 +1,17 @@
+package aula02_10102017;
+
+/**
+ * Just testing the GitHub funcionality
+ */
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UdpCliTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
