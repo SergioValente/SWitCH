36fb2bcd954b9a119f34cf518295e4efb6d89d4a
SergioValente <sergio134@sapo.pt>
2017-10-18 15:24:22 +0100

    yet another optimization of TestSudokuSolution class

--- DEVSOFT1/src/aula06_18102017/TestSudokuSolution.java
+++ DEVSOFT1/src/aula06_18102017/TestSudokuSolution.java
@@ -11,91 +11,13 @@ public class TestSudokuSolution {
 	 */
 	public static boolean isValidSudokuSolution (int[][] jogo) {
 		
-		if(!isMatrizNovePorNove(jogo)) return false;
+		if (!isMatrizNovePorNove(jogo)) return false;
+		if(!matrizTemNumerosTodosValidos(jogo)) return false;
 		if (!linhasCorretas(jogo)) return false;
-		if(!colunasCorretas(jogo)) return false;
-		if(!matrizesInternasCorretas(jogo)) return false;
+		if (!colunasCorretas(jogo)) return false;
+		if (!matrizesInternasCorretas(jogo)) return false;
 		
 		return true;	
-	}
-	
-	/**
-	 * Método para pegar numa matriz interna do jogo Sudoku e passar para um vetor	
-	 * @param a matriz completa
-	 * @param x índice da linha
-	 * @param y índice da coluna
-	 * @return matriz interna na forma de um vetor
-	 */
-	private static int[] converteMatrizInternaParaVetor(int[][] jogo, int x, int y) {
-
-		int indexVector = 0;
-		int[] matrixToVector = new int[9];
-
-		for (int i = x; i < x + 3; i++) {
-			for (int j = y; j < y + 3; j++) {
-				matrixToVector[indexVector++] = jogo[i][j];
-			}
-		}
-		return matrixToVector;
-	}
-	
-	/**
-	 * Método para verificar se as linhas da matriz estão corretas
-	 * @param jogo matriz do jogo
-	 * @return true se todas as linhas estão corretas, false se pelo menos uma delas falha
-	 */
-	private static boolean linhasCorretas(int[][] jogo) {
-		
-		boolean temDuplicadosLinha = false;
-		
-		for (int i = 0; i < jogo.length; i ++) {
-			
-			//se matriz não tem o número correto de linhas, retorna false
-			if(jogo[i].length != jogo.length) return false;  
-			
-			temDuplicadosLinha = OperacoesSobreUmVetor.hasDuplicates(jogo[i]);
-			if (temDuplicadosLinha) return false;
-		}
-		return true;
-	}
-	
-	/**
-	 * Método para verificar se colunas da matriz estão corretas
-	 * @param jogo matriz do jogo
-	 * @return true se todas as colunas estão corretas, false se pelo menos uma delas falha
-	 */
-	private static boolean colunasCorretas(int[][] jogo) {
-		
-		boolean temDuplicadosColuna = false;
-		int[][] transposta = OperacoesSobreMatrizes.matrizTransposta(jogo);
-		
-		for (int i = 0; i < jogo.length; i ++) {
-			
-			//se matriz não tem o número correto de colunas, retorna false
-			if(jogo[i].length != jogo.length) return false;
-			
-			temDuplicadosColuna = OperacoesSobreUmVetor.hasDuplicates(transposta[i]);
-			if (temDuplicadosColuna) return false;
-		}
-		return true;
-	}
-	
-	/**
-	 * Método que verifica se as matrizes internas 3X3 estão corretas
-	 * @param jogo matriz completa do jogo
-	 * @return true se as matrizes internas estão corretas, false se não estão corretas
-	 */
-	private static boolean matrizesInternasCorretas(int[][] jogo) {
-		
-		int[] vetorMatrizInterna;
-		
-		for (int i = 0; i < jogo.length; i += 3) {
-			for (int j = 0; j < jogo.length; j += 3) {
-				vetorMatrizInterna = converteMatrizInternaParaVetor(jogo, i, j);
-				if (OperacoesSobreUmVetor.hasDuplicates(vetorMatrizInterna)) return false;
-			}
-		}
-		return true;
 	}
 	
 	/**
@@ -108,6 +30,101 @@ public class TestSudokuSolution {
 		if(jogo.length != 9 || jogo[0].length != 9) return false;
 		
 		return true;
+	}
+	
+	/**
+	 * Método para verificar se todos os números na matriz estão entre 0 e 9
+	 * @param jogo matriz a verificar
+	 * @return true se todos os números são válidos, false se pelo menos um está fora do intervalo 
+	 */
+	private static boolean matrizTemNumerosTodosValidos(int[][] jogo) {
+		
+		for(int i = 0; i < jogo.length; i++) {
+			for(int j = 0; j < jogo[i].length; j++) {
+				if (jogo[i][j] < 1 || jogo[i][j] > 9) return false;
+			}
+		}
+		return true;
 	}	
+
+	/**
+	 * Método para verificar se as linhas da matriz estão corretas
+	 * @param jogo matriz do jogo
+	 * @return true se todas as linhas estão corretas, false se pelo menos uma delas falha
+	 */
+	private static boolean linhasCorretas(int[][] jogo) {
+		
+		boolean temDuplicados = false;
+		
+		for (int i = 0; i < jogo.length; i ++) {
+			
+			//se matriz não tem o número correto de colunas, retorna false
+			if(jogo[i].length != jogo.length) return false;  
+			
+			temDuplicados = OperacoesSobreUmVetor.hasDuplicates(jogo[i]);
+			if (temDuplicados) return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Método para verificar se colunas da matriz estão corretas através da transposta
+	 * @param jogo matriz do jogo
+	 * @return true se todas as colunas estão corretas, false se pelo menos uma delas falha
+	 */
+	private static boolean colunasCorretas(int[][] jogo) {
+		
+		int[][] transposta = OperacoesSobreMatrizes.matrizTransposta(jogo);
+		
+		return (linhasCorretas(transposta));
+	}
+	
+	/**
+	 * Método que verifica se as matrizes internas 3X3 estão corretas
+	 * @param jogo matriz completa do jogo
+	 * @return true se as matrizes internas estão corretas, false se não estão corretas
+	 */
+	private static boolean matrizesInternasCorretas(int[][] jogo) {
+		
+		return (linhasCorretas(unwindMatriz(jogo)));
+	}
+	
+	/**
+	 * Método para pegar numa matriz interna do jogo Sudoku e passar para um vetor	
+	 * @param a matriz completa
+	 * @param x índice da linha
+	 * @param y índice da coluna
+	 * @return matriz interna na forma de um vetor
+	 */
+	private static int[] converteMatrizInternaParaVetor(int[][] jogo, int x, int y) {
+	
+		int indexVector = 0;
+		int[] matrixToVector = new int[9];
+	
+		for (int i = x; i < x + 3; i++) {
+			for (int j = y; j < y + 3; j++) {
+				matrixToVector[indexVector++] = jogo[i][j];
+			}
+		}
+		return matrixToVector;
+	}
+	
+	/**
+	 * Método que converte as sub-matrizes 3x3 em linhas de uma matriz 9x9
+	 * @param jogo matriz para converter
+	 * @return matriz 9x9 "desembrulhada"
+	 */
+	private static int[][] unwindMatriz(int[][] jogo) {
+		
+		int[][] matrizDesembrulhada = new int[9][9]; 
+		int indice = 0;
+		
+		for (int i = 0; i < jogo.length; i += 3) {
+			for (int j = 0; j < jogo[i].length; j += 3) {
+				matrizDesembrulhada[indice++] = converteMatrizInternaParaVetor(jogo, i, j);
+			}
+		}
+		return matrizDesembrulhada;
+	}
 }
 
--- DEVSOFT1/tests/aula06_18102017/TestSudokuSolutionTest.java
+++ DEVSOFT1/tests/aula06_18102017/TestSudokuSolutionTest.java
@@ -10,14 +10,14 @@ public class TestSudokuSolutionTest {
 	public void testIsValidSudokuSolutionTrue() {
 		
 		int [][] a = {{8,6,2,7,9,5,3,4,1}, 
-				  {7,4,9,6,3,1,5,2,8},
-				  {5,1,3,8,4,2,9,6,7},
-				  {3,7,8,9,2,6,1,5,4},
-				  {1,2,4,5,8,7,6,9,3},
-				  {6,9,5,4,1,3,8,7,2},
-				  {4,5,1,2,6,8,7,3,9},
-				  {2,8,6,3,7,9,4,1,5},
-				  {9,3,7,1,5,4,2,8,6}};
+				  	  {7,4,9,6,3,1,5,2,8},
+				  	  {5,1,3,8,4,2,9,6,7},
+				  	  {3,7,8,9,2,6,1,5,4},
+				  	  {1,2,4,5,8,7,6,9,3},
+				  	  {6,9,5,4,1,3,8,7,2},
+				  	  {4,5,1,2,6,8,7,3,9},
+				  	  {2,8,6,3,7,9,4,1,5},
+				  	  {9,3,7,1,5,4,2,8,6}};
 	boolean result = TestSudokuSolution.isValidSudokuSolution(a);
 	
 	assertTrue(result);	
@@ -26,14 +26,14 @@ public class TestSudokuSolutionTest {
 	@Test
 	public  void testIsValidSudokuSolutionFalse() {
 		int [][] a = {{8,6,2,7,9,5,3,4,1}, 
-				  {7,4,9,6,3,1,5,2,8},
-				  {5,1,3,8,4,2,9,6,7},
-				  {3,7,8,9,4,6,1,5,4}, //falso nesta linha, o 1º 4 deve ser um 2
-				  {1,2,4,5,8,7,6,9,3},
-				  {6,9,5,4,1,3,8,7,2},
-				  {4,5,1,2,6,8,7,3,9},
-				  {2,8,6,3,7,9,4,1,5},
-				  {9,3,7,1,5,4,2,8,6}};
+				  	  {7,4,9,6,3,1,5,2,8},
+				  	  {5,1,3,8,4,2,9,6,7},
+				  	  {3,7,8,9,4,6,1,5,4}, //falso nesta linha, o 1º 4 deve ser um 2
+				  	  {1,2,4,5,8,7,6,9,3},
+				  	  {6,9,5,4,1,3,8,7,2},
+				  	  {4,5,1,2,6,8,7,3,9},
+				  	  {2,8,6,3,7,9,4,1,5},
+				  	  {9,3,7,1,5,4,2,8,6}};
 	boolean result = TestSudokuSolution.isValidSudokuSolution(a);
 	
 	assertFalse(result);
@@ -63,5 +63,23 @@ public class TestSudokuSolutionTest {
 		boolean result = TestSudokuSolution.isValidSudokuSolution(a);
 		
 		assertFalse(result);
+	}
+
+	
+	@Test
+	public  void testIsValidSudokuSolutionNumerosInvalidos() {
+	
+		int [][] a = {{8,6,2,7,9,5,3,4,1}, 
+			  	  	  {7,4,9,6,3,1,5,2,8},
+			  	  	  {5,1,3,8,4,2,9,6,7},
+			  	  	  {3,7,8,9,2,6,1,5,4},
+			  	  	  {1,2,4,5,8,7,6,9,3},
+			  	  	  {6,9,5,4,1,3,8,7,2},
+			  	  	  {4,5,1,2,6,8,12,3,9}, // linha com o número 12
+			  	  	  {2,8,6,3,7,9,4,1,5},
+			  	  	  {9,3,7,1,5,4,2,8,6}};
+		boolean result = TestSudokuSolution.isValidSudokuSolution(a);
+
+		assertFalse(result);	
 	}
 }
